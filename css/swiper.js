f96deab8db311c72da8d576fa06079df524b02a1
Dikshamahi06 <mahidiksha7@gmail.com>
2024-09-09 16:57:50 +0530

    first commit

---
+++ css/swiper.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    effect: "creative",
+    creativeEffect: {
+        prev: {
+            translate: [0, 0, -400],
+        },
+        next: {
+            translate: ["100%", 0, 0],
+        },
+    },
+    loop: true,
+    direction: "horizontal",
+    autoplay:{
+        delay: 5000,
+    },
+    speed: 200 ,
+    spaceBetween: 100,
+
+});
